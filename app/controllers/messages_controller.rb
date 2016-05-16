7938c81ea48ce3fe836f658d018cb18e5ec076a2
keisukeTerada <terada@basicinc.jp>
2016-05-16 03:52:44 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,32 @@
 class MessagesController < ApplicationController
   #indexというアクションに対応した処理を書くことができる（これメソッドだったんだ笑）
   def index
+    #おおわかるぞ，このクラスのクラスインスタンスmessageを定義してMessageクラスはあらかじめ作ってあるのだな
+    #indexにアクセスした際にメッセージオブジェクトの初期化を行うことで 
+    #メッセージの新規作成フォームが表示できるようになる　＝しないとオブジェクト（URL）がないのでアクセスできない
+    @message = Message.new
+    ##ここで1件のメッセージを作るとMessage.allの方に1けんのデータを追加できる
+    #そしてその全てのデータを@messages　（複数）のやつに代入することで値を表示することができる
+    #ここからindexに向かって値を渡していることがわかった
     
+    #現在保存されている全てのMessageオブジェクトの内容を返すようにできている
+    #なのでいつも通りeach分とか使ってぶん回すと楽しい
+    @messages = Message.all
+  end
+  
+  def create
+    #                     モデル名の小文字_paramsで引数を受け取ることができる
+    @message = Message.new(message_params)
+    @message.save
+    #ここでさっきのヘルパを利用しているのですね
+    redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+  
+  
+  #入力内容の正規化　不正防止
+  #送られてくる変数名をメソッド名に指定して params.reqire(:message).premit（：受け取りたいやつA：B）といった感じに書く
+  private
+  def message_params
+    params.require(:message).permit(:name,:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
   # rake routes  で現在のルート確認ができるよ
   root 'messages#index'
+  #どのURLに対してどのコントローラーのどのアクションなのかを設定している
+  
+  #resourcesとはリソースを操作するURLを作るメソッドである
+  #messagesモントローラーのcreateアクションかな
+  #今回の場合はメッセージというリソースのクリエイトのみを作成してくださいって書いてるから
+  
+  #resources :messages , only: [:create]
+  
+  #messages POST /messages(.:format) messages#create　　こんな感じでクリエイトだけ出来るわけで
+  #いかのようにこれだけ書いたら　一覧表示，作成処理，新規作成，編集，個別画面，更新処理，削除処理全部作ってくれちゃうわけですね
+  resources :messages
+  #ここで使えるヘルパの名前も作ってくれる
+  #これがあると　/messages/:id/editとか入力しなくても edit_message_pathといった感じで使えるので覚えやすい？
+  #ヘルパ名	対応するHTTPリクエスト名	出力されるURL
+  #messages_path	GET/POST	/messages
+  #new_message_path	GET	/messages/new
+  #edit_message_path	GET	/messages/:id/edit
+  #message_path(id)	GET/PATCH/PUT/DELETE	/messages/:id
+  
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
